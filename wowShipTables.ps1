933da3c348094a3ff52c1600525fd63d4a92b4bb
Brad <bradleynielsen@gmail.com>
2024-08-25 18:34:48 -0500

    first

---
+++ wowShipTables.ps1
@@ -0,0 +1,67 @@
+﻿$uri = "https://api.worldofwarships.com/wows/encyclopedia/ships/?application_id=0461d545fdf71ee8d9daffdd24c62438&page_no=1"
+$baseuri = "https://api.worldofwarships.com/wows/encyclopedia/ships/?application_id=0461d545fdf71ee8d9daffdd24c62438&page_no="
+$response =  Invoke-RestMethod -Uri $uri -Method Get
+$meta =  $response.meta 
+$data =  $response.data 
+
+#get page count
+$shiptotal = $meta.total
+$pagesize = $shiptotal / 100
+$pagecount = [math]::Ceiling($pagesize)
+
+
+# Initialize an empty array to store the data
+$shipsTable = @()
+
+
+#get each page
+
+# For loop that runs based on the value of $loopSize
+for ($i = 0; $i -lt $pagecount; $i++) {
+
+    #set page number
+    $uripaged = $baseuri + $pagecount
+
+
+    $response =  Invoke-RestMethod -Uri $uripaged -Method Get
+
+
+    $shipData = $response.data.PSObject.Properties.Value
+
+
+    foreach ($ship in $shipData) {
+
+
+        # Create a PSCustomObject for each ship with relevant properties
+        $shipObject = [PSCustomObject]@{
+            ShipID            = $ship.ship_id
+            Name              = $ship.name
+            Nation            = $ship.nation
+            Type              = $ship.type
+            Tier              = $ship.tier
+            Hitpoints         = $ship.default_profile.armour.health
+            TorpReloadTimeSec = $ship.default_profile.torpedoes.reload_time
+            TorpFiringRange   = $ship.default_profile.torpedoes.distance
+            NumMainTurrets    = $ship.default_profile.hull.artillery_barrels
+            FiringRange       = $ship.default_profile.fire_control.distance
+            ReloadTimesec     = $ship.default_profile.atbas.slots.shot_delay
+            ShellType         = $ship.default_profile.atbas.slots.type
+        }
+
+        # Add the ship object to the array
+        $shipsTable += $shipObject
+    }
+
+
+}
+
+
+
+
+$shipsTable | Format-Table -AutoSize
+
+# Determine the path to save the CSV
+$outputFilePath = Join-Path -Path $PSScriptRoot -ChildPath "ships.csv"
+
+# Export the table to a CSV file in the script's directory
+$shipsTable | Export-Csv -Path $outputFilePath -NoTypeInformation
---
+++ wowShipTables2024-08-25.ps1
@@ -0,0 +1,198 @@
+﻿#region init
+
+    # init
+    $rootPath = $PSScriptRoot
+    $shipsIdList = @()
+    $shipsTable  = @()
+    $uri         = "https://api.worldofwarships.com/wows/encyclopedia/ships/?application_id=0461d545fdf71ee8d9daffdd24c62438&page_no=1"
+    $baseuri     = "https://api.worldofwarships.com/wows/encyclopedia/ships/?application_id=0461d545fdf71ee8d9daffdd24c62438&page_no="
+
+
+
+    #get list of attributes
+    $attributesCSV = Import-Csv -Path $PSScriptRoot"\attributes.csv"
+    $attributes    = $attributesCSV.Field
+
+    #add field descriptions to $shipsTable
+    $fieldDescriptions = & ($rootPath+"\fieldDescriptions.ps1")
+    $shipsTable       += $fieldDescriptions
+
+
+#endregion init
+
+
+#region functions
+
+    function extractFieldValue{
+        param(
+            $response,
+            $field
+        )
+        $value = $response.data.PSObject.Properties.Value.$field
+        return $value
+    }
+
+    function sendFieldRequest{
+        param(
+            $shipID,
+            $field
+        )
+
+        #build request string
+        #example: https://api.worldofwarships.com/wows/encyclopedia/ships/?application_id=0461d545fdf71ee8d9daffdd24c62438&page_no=1&ship_id=3248404240&fields=default_profile.battle_level_range_min
+        $URI = "https://api.worldofwarships.com/wows/encyclopedia/ships/?application_id=0461d545fdf71ee8d9daffdd24c62438&page_no=1&ship_id=$shipID&fields=$field"
+        $response = Invoke-RestMethod -Uri $URI -Method Get
+        return $response
+    }
+
+
+
+#endregion functions
+
+
+    
+
+
+
+
+#region build ship list
+
+    #get meta info 
+    $response =  Invoke-RestMethod -Uri $uri -Method Get
+    $meta     =  $response.meta 
+
+    #get page count
+    $shiptotal = $meta.total
+    $pagesize  = $shiptotal / 100
+    $pagecount = [math]::Ceiling($pagesize) + 1
+
+    # For loop that runs based on the value of $loopSize
+    for ($i = 1; $i -lt $pagecount; $i++) {
+        
+        "getting page " +$i
+
+        #set page number
+        $uripaged = $baseuri + $i
+        
+        #request ids and add to array
+        $response     = Invoke-RestMethod -Uri $uripaged -Method Get
+        $shipids      = extractFieldValue -response $response -field "ship_id"
+        $shipsIdList += $shipids
+    }
+
+#endregion
+
+
+
+#region build ship table
+
+    #loop through shipsIdList
+    foreach ($shipID in $shipsIdList) {
+        "processing $shipID"
+
+        #init $shipObject
+        $response = sendFieldRequest -shipID $shipID -field "description"
+
+        $description =  extractFieldValue -response $response -field "description"
+        $shipObject  = [PSCustomObject]@{
+            description = $description
+        }
+
+        #loop through attributes
+        foreach ($attribute in $attributes) {
+            "fetching attribute: "+$attribute
+            #generate a new request for the attribute
+            $response = sendFieldRequest -shipID $shipID -field $attribute
+
+            #get attribute value
+            $value = extractFieldValue -response $response -field $attribute
+
+            # Add a new key-value pair using Add-Member
+            $shipObject | Add-Member -MemberType NoteProperty -Name $attribute -Value $value -ErrorAction SilentlyContinue
+        }
+
+        #display final ship object
+        $shipObject
+    }
+
+
+
+
+#endregion
+
+
+
+
+
+
+
+
+<#
+
+
+
+
+
+
+
+
+
+
+        foreach ($ship in $shipData) {
+            foreach($property in $ship.PSObject.Properties){
+            $name = $property.Name
+            $value = $property.Value
+            Write-Host "${name}: $value"
+
+        }
+
+
+
+
+            # Add the ship object to the array
+            $shipsTable += $shipObject
+
+
+
+
+
+
+
+
+$shipsTable | Format-Table -AutoSize
+
+# Determine the path to save the CSV
+$outputFilePath = Join-Path -Path $PSScriptRoot -ChildPath "ships.csv"
+
+# Export the table to a CSV file in the script's directory
+$shipsTable | Export-Csv -Path $outputFilePath -NoTypeInformation
+
+
+
+
+
+
+
+
+
+            # Create a PSCustomObject for each ship with relevant properties
+            $shipObject = [PSCustomObject]@{
+                ShipID            = $ship.ship_id
+                Name              = $ship.name
+                Nation            = $ship.nation
+                Type              = $ship.type
+                Tier              = $ship.tier
+                Hitpoints         = $ship.default_profile.armour.health
+                TorpReloadTimeSec = $ship.default_profile.torpedoes.reload_time
+                TorpFiringRange   = $ship.default_profile.torpedoes.distance
+                NumMainTurrets    = $ship.default_profile.hull.artillery_barrels
+                FiringRange       = $ship.default_profile.fire_control.distance
+                ReloadTimesec     = $ship.default_profile.atbas.slots.shot_delay
+                ShellType         = $ship.default_profile.atbas.slots.type
+            }
+
+#>
+
+
+
+
