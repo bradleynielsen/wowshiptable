09e548ad05b103af170fc58261d5cf212f9cedfd
Brad <bradleynielsen@gmail.com>
2024-08-26 12:44:41 -0500

    saving before fn combine

--- wowShipTables2024-08-25.ps1
+++ wowShipTables2024-08-25.ps1
@@ -7,8 +7,6 @@
     $uri         = "https://api.worldofwarships.com/wows/encyclopedia/ships/?application_id=0461d545fdf71ee8d9daffdd24c62438&page_no=1"
     $baseuri     = "https://api.worldofwarships.com/wows/encyclopedia/ships/?application_id=0461d545fdf71ee8d9daffdd24c62438&page_no="
 
-
-
     #get list of attributes
     $attributesCSV = Import-Csv -Path $PSScriptRoot"\attributes.csv"
     $attributes    = $attributesCSV.Field
@@ -23,7 +21,7 @@
 
 #region functions
 
-    function extractFieldValue{
+    function extractFieldValue {
         param(
             $response,
             $field
@@ -32,7 +30,7 @@
         return $value
     }
 
-    function sendFieldRequest{
+    function sendFieldRequest {
         param(
             $shipID,
             $field
@@ -40,19 +38,26 @@
 
         #build request string
         #example: https://api.worldofwarships.com/wows/encyclopedia/ships/?application_id=0461d545fdf71ee8d9daffdd24c62438&page_no=1&ship_id=3248404240&fields=default_profile.battle_level_range_min
-        $URI = "https://api.worldofwarships.com/wows/encyclopedia/ships/?application_id=0461d545fdf71ee8d9daffdd24c62438&page_no=1&ship_id=$shipID&fields=$field"
+        $URI      = "https://api.worldofwarships.com/wows/encyclopedia/ships/?application_id=0461d545fdf71ee8d9daffdd24c62438&page_no=1&ship_id=$shipID&fields=$field"
         $response = Invoke-RestMethod -Uri $URI -Method Get
         return $response
     }
 
+    function exportShipTable{
+        Param(
+            $rootPath,
+            $shipsTable
+        )
+
+        # Determine the path and file name to save the CSV
+        $outputFilePath = Join-Path -Path $PSScriptRoot -ChildPath "ships.csv"
+
+        # Export the table to a CSV file in the script's directory
+        $shipsTable | Export-Csv -Path $outputFilePath -NoTypeInformation
+    }
 
 
 #endregion functions
-
-
-    
-
-
 
 
 #region build ship list
@@ -83,7 +88,6 @@
 #endregion
 
 
-
 #region build ship table
 
     #loop through shipsIdList
@@ -100,12 +104,13 @@
 
         #loop through attributes
         foreach ($attribute in $attributes) {
-            "fetching attribute: "+$attribute
+            "fetching attribute: "+$attribute+": "
             #generate a new request for the attribute
             $response = sendFieldRequest -shipID $shipID -field $attribute
 
             #get attribute value
             $value = extractFieldValue -response $response -field $attribute
+            Write-Host -NoNewline $value
 
             # Add a new key-value pair using Add-Member
             $shipObject | Add-Member -MemberType NoteProperty -Name $attribute -Value $value -ErrorAction SilentlyContinue
@@ -113,50 +118,17 @@
 
         #display final ship object
         $shipObject
+
+        # Add the ship object to the array
+        $shipsTable += $shipObject
+        exportShipTable -rootPath $rootPath -shipsTable $shipsTable
     }
-
-
-
 
 #endregion
 
 
 
-
-
-
-
-
 <#
-
-
-
-
-
-
-
-
-
-
-        foreach ($ship in $shipData) {
-            foreach($property in $ship.PSObject.Properties){
-            $name = $property.Name
-            $value = $property.Value
-            Write-Host "${name}: $value"
-
-        }
-
-
-
-
-            # Add the ship object to the array
-            $shipsTable += $shipObject
-
-
-
-
-
-
 
 
 $shipsTable | Format-Table -AutoSize
@@ -166,13 +138,6 @@ $outputFilePath = Join-Path -Path $PSScriptRoot -ChildPath "ships.csv"
 
 # Export the table to a CSV file in the script's directory
 $shipsTable | Export-Csv -Path $outputFilePath -NoTypeInformation
-
-
-
-
-
-
-
 
 
             # Create a PSCustomObject for each ship with relevant properties
